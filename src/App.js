a73f885351fd002d9b9e6e68df11d94921899428
mansarip <luqmanrasa@gmail.com>
2019-08-09 16:27:39 +0800

    fix incomplete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Button } from "antd";
 
 export default function App() {
-    return <h1>Craco Antd Starter</h1>;
+  return (
+    <>
+      <h1>Craco Antd Starter</h1>
+      <Button>Sample Button</Button>
+    </>
+  );
 }
